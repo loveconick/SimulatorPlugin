4755d3430339e4071b810b099756b108c6b3f189
loveconick <21597375@qq.com>
2018-04-11 15:11:10 +0800

    main.c->main.m
    为了使用objc_msgSend时能够通过编译

--- main.m
+++ main.m
@@ -14,7 +14,29 @@
 #include <sys/sysctl.h>
 #include <dlfcn.h>
 
+//#include <objc/objc-runtime.h>
+#include <objc/message.h>
+
 #include "plugin.h"
+
+
+void PrintRegister(CPUAL* cpual)
+{
+	plugin_msg("0x%016llX 0x%016llX %p %p %p %p %p"
+			   , cpual->free, cpual->ret_code, cpual->vmcpu, cpual->cpu
+			   , cpual->tmp_ctx, cpual->sp_ptr, cpual->stack_top);
+	plugin_msg("pc==0x%016llX, lr==0x%016llX , sp==0x%016llX, 0x%016llX"
+			   ,cpual->gp->pc,cpual->gp->r[30],cpual->gp->uc_sp,cpual->gp->sim_sp);
+	int i = 0;
+	for (i=0; i<24; i+=8) {
+		plugin_msg("%02d 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX",i
+				   ,cpual->gp->r[i+0],cpual->gp->r[i+1],cpual->gp->r[i+2],cpual->gp->r[i+3]
+				   ,cpual->gp->r[i+4],cpual->gp->r[i+5],cpual->gp->r[i+6],cpual->gp->r[i+7]);
+	}
+	plugin_msg("%02d 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX 0x%016llX",i
+			   ,cpual->gp->r[i+0],cpual->gp->r[i+1],cpual->gp->r[i+2],cpual->gp->r[i+3]
+			   ,cpual->gp->r[i+4],cpual->gp->r[i+5],cpual->gp->r[i+6]);
+}
 
 void my_stub_handler(CPUAL* cpual)
 {
@@ -23,31 +45,22 @@ void my_stub_handler(CPUAL* cpual)
 	//const char      *dli_sname;     /* Name of nearest symbol */
 	//void            *dli_saddr;     /* Address of nearest symbol */
 	Dl_info dlinfo;
-	dladdr((const void *)cpual->gp->pc, &dlinfo);
-	plugin_msg("%s %s", dlinfo.dli_fname, dlinfo.dli_sname);
+	if(cpual->gp->pc == (uint64_t)objc_msgSend)
+		plugin_msg("objc method %s", (char *)cpual->gp->r[1]);
+	else{
+		dladdr((const void *)cpual->gp->pc, &dlinfo);
+		plugin_msg("%s %s", dlinfo.dli_fname, dlinfo.dli_sname);
+	}
+	PrintRegister(cpual);
 	
-    bool bHit = false;
-    //plugin_msg("call stub lr: 0x%llx\n", cpual->gp->r[30]);
+    //plugin_msg("call stub lr: 0x0x%016llX\n", cpual->gp->r[30]);
     if(cpual->gp->pc == (uint64_t)fopen){
-        bHit = true;
         plugin_msg("call fopen(%s %s)", (char *)cpual->gp->r[0], (char *)cpual->gp->r[1]);
     }else if (cpual->gp->pc == (uint64_t)sysctlbyname) {
-        bHit = true;
-        plugin_msg("call sysctlbyname(%s), lr %llx", cpual->gp->r[0], cpual->gp->r[30]);
+        plugin_msg("call sysctlbyname(%s), lr 0x%016llX", cpual->gp->r[0], cpual->gp->r[30]);
 	}else if (cpual->gp->pc == (uint64_t)1) {	//objc_msgSend
-		bHit = true;
 		plugin_msg("call fprintf : %s %d", (char *)cpual->gp->r[2], (int)cpual->gp->r[3]);
 	}
-	
-    /*if (bHit) {
-        plugin_msg("%08X %08X %p %p %p %p %p"
-			, cpual->free, cpual->ret_code, cpual->vmcpu, cpual->cpu
-				   , cpual->tmp_ctx, cpual->sp_ptr, cpual->stack_top);
-		plugin_msg("%08X %08X %08X",cpual->gp->pc,cpual->gp->uc_sp,cpual->gp->sim_sp);
-		for (int i=0; i<31; i++) {
-			plugin_msg("%02d %08X",i,cpual->gp->r[i]);
-		}
-    }*/
 }
 
 //###################################################
